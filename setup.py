2b866f8bd8ec06ffe8c4b7b73d2ac618aaacfd29
Aspen Schneider <hardboiled65@gmail.com>
2024-01-30 10:17:10 +0900

    Preparing for class based setup script.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os, sys, shutil
+from abc import ABC, abstractmethod
 
 def print_intro(msg):
     print('=== {} ==='.format(msg))
@@ -45,6 +46,159 @@ if PLATFORM == 'windows':
     PLATFORM_HOME = windows_home()
 else:
     PLATFORM_HOME = unix_home()
+
+DEV_ENVIRONMENT_DIR = os.path.dirname(__file__)
+
+
+class Setting(ABC):
+    def platform(self) -> str:
+        return PLATFORM
+
+    def home(self) -> str:
+        return PLATFORM_HOME
+
+    @abstractmethod
+    def check(self) -> bool:
+        """Check if the program installed and ready to install settings."""
+        pass
+
+    @abstractmethod
+    def install_method(self) -> str:
+        """Install method. 'system' | 'tar' | 'flatpak'"""
+        pass
+
+    @abstractmethod
+    def installed(self) -> bool:
+        """Check if setting files installed."""
+        pass
+
+    @abstractmethod
+    def conflicted(self) -> bool:
+        """Check if files conflicted."""
+        return True
+
+    @abstractmethod
+    def backup(self):
+        pass
+
+    @abstractmethod
+    def install(self):
+        pass
+
+
+class Vim(Setting):
+    def __init__(self):
+        pass
+
+    def check(self) -> bool:
+        return shutil.which('vim') is not None
+
+    def install_method(self) -> str:
+        return 'system'
+
+    def installed(self) -> bool:
+        if os.path.islink((os.getenv('HOME') + '/.vimrc')):
+            return True
+        return False
+
+    def conflicted(self) -> bool:
+        if os.path.isfile(os.path.join(self.home(), '.vimrc')):
+            return True
+        return False
+
+    def backup(self):
+        if os.path.isfile(os.path.join(self.home(), '.vimrc')):
+            shutil.move(os.path.join(self.home(), '.vimrc'),
+                os.path.join(self.home(), '.vimrc.old'))
+
+    def install(self):
+        os.symlink(os.path.join(DEV_ENVIRONMENT_DIR, 'vimrc'),
+            os.path.join(self.home(), '.vimrc'))
+
+
+class Zsh(Setting):
+    def __init__(self):
+        pass
+
+    def check(self) -> bool:
+        return shutil.which('zsh') is not None
+
+    def install_method(self) -> str:
+        return 'system'
+
+    def installed(self) -> bool:
+        if os.path.isdir(os.path.join(self.home() + '.oh-my-zsh')):
+            return True
+        return False
+
+
+class Code(Setting):
+    def __init__(self) -> None:
+        pass
+
+    def check(self) -> bool:
+        if self.platform() == 'macos':
+            target_dir = os.path.join(self.home(), 'Library/Application Support/Code/User')
+            if os.path.isdir(target_dir):
+                return True
+            else:
+                return False
+        elif self.platform() == 'linux':
+            target_dir = ''
+            if self.install_method() == 'tar':
+                target_dir = os.path.join(self.home(), '.config/Code/User')
+            elif self.install_method() == 'flatpak':
+                target_dir = os.path.join(self.home(), '.var/app/com.visualstudio.code/config/Code/User')
+            else:
+                return False
+
+            if os.path.isdir(target_dir):
+                return True
+            else:
+                return False
+        elif self.platform() == 'windows':
+            return False    # TODO: Implementation.
+
+    def install_method(self) -> str:
+        if self.platform() in ('windows', 'macos'):
+            return 'system'
+        else:
+            tar_path = os.path.join(self.hone(), '.config/Code/User')
+            flatpak_path = os.path.join(self.home(), '.var/app/com.visualstudio.code')
+            if os.path.isdir(tar_path):
+                return 'tar'
+            elif os.path.isdir(flatpak_path):
+                return 'flatpak'
+            else:
+                return 'unknown'
+
+    def installed(self) -> bool:
+        settings_json = ''
+        keybindings_json = ''
+
+        if self.platform() == 'linux':
+            target_dir = ''
+            if self.install_method() == 'tar':
+                target_dir = '.config/Code/User'
+            elif self.install_method() == 'flatpak':
+                target_dir = '.var/app/com.visualstudio.code/config/Code/User'
+            settings_json = os.path.join(self.home(), target_dir, 'settings.json')
+            keybindings_json = os.path.join(self.home(), target_dir, 'keybindings.json')
+
+            if os.path.islink(settings_json) and os.path.islink(keybindings_json):
+                return True
+            return False
+        elif self.platform() == 'macos':
+            target_dir = 'Library/Application Support/Code/User'
+            settings_json = os.path.join(self.home(), target_dir, 'settings.json')
+            keybindings_json = os.path.join(self.home(), target_dir, 'keybindings.json')
+
+            if os.path.islink(settings_json) and os.path.islink(keybindings_json):
+                return True
+            return False
+        elif self.platform() == 'windows':
+            return False
+
 
 settings = {
     'vim': {
@@ -99,20 +253,25 @@ OH_MY_ZSH_CURL = 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrus
 OH_MY_ZSH_WGET = 'sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
 
 # VIM config.
+vim = Vim()
+
 print_intro('VIM')
 
-if os.path.islink(paths['VIMRC']):
-    print('.vimrc file is already linked.')
+if vim.installed():
+    print('.vimrc file is already linked')
     print_done()
-
-if not os.path.islink(paths['VIMRC']) and confirm('Install?', default_yes=True):
-    if os.path.isfile(paths['VIMRC']):
-        print('.vimrc file already exists.')
-        if confirm('Do you want to make backup?', default_yes=True):
-            shutil.move(paths['VIMRC'], paths['VIMRC'] + '.old')
-            print('file moved: .vimrc => .vimrc.old')
-    os.symlink('dev-environment/vimrc', paths['VIMRC'])
-    print_done()
+elif vim.check():
+    print('VIM settings ready to be installed.')
+    if confirm('Install?', default_yes=True):
+        if vim.conflicted():
+            print('.vimrc file is already exists.')
+            if confirm('Do you want to make backup?', default_yes=True):
+                vim.backup()
+                print('File moved: .vimrc => .vimrc.old')
+        vim.install()
+        print_done()
+else:
+    print('VIM is not ready. Aborted.')
 
 # Sublime Text 3 settings.
 print_intro('Sublime Text 3')
